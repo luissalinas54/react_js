81118b59e5e9858c2a6e17bc945d4addca3a6b7c
luissalinas54 <luisasb1712@gmail.com>
2026-05-11 23:13:35 -0500

    Valores Dinamicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+const reactTitles = [
+  "React Fundamentos",
+  "React Principios",
+  "React Conceptos",
+];
+
+function getRandomIntInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function Header() {
+  const titleReact =
+    reactTitles[getRandomIntInRange(0, reactTitles.length - 1)];
   return (
     <header>
       <img src="src/assets/react-logo-xs.png" alt="React logo" />
-      <h1>React Prueba</h1>
+      <h1>{titleReact}</h1>
       <p>
         ¡Conceptos fundamentales de React que necesitas conocer para desarrollar
         cualquier app con esta famosa librería!
